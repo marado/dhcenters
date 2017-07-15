67225b97e76424c29063ae8d19dd8f2804fe0a9d
Marcos Marado <mindboosternoori@gmail.com>
2017-07-15 18:40:43 +0100

    Force HTML parsing in UTF8
    Closes: #1

--- dhcenters.sh
+++ dhcenters.sh
@@ -11,9 +11,9 @@ echo "Center,Location" > dhcenters.csv
 for i in $(seq 0 $lastpage); do
   wget "http://dhcenternet.org/centers?page=$i" -O "$i.html" 
   cat $i.html |hxnormalize -x -l 1000|hxselect tbody|hxselect div.views-field-realname|hxselect span.field-content|sed 's/<span/\n\<span/g'|grep -v ^$ > "$i.names"
-  for a in $(seq 1 $(cat $i.names|wc -l)); do cat $i.names|head -n$a|tail -n1|hxselect -c a|html2text -width 1000; done > $i.parsednames
+  for a in $(seq 1 $(cat $i.names|wc -l)); do cat $i.names|head -n$a|tail -n1|hxselect -c a|html2text -utf8 -width 1000; done > $i.parsednames
   cat $i.html |hxnormalize -x -l 1000|hxselect tbody|hxselect div.views-field-field-location|grep field-content > "$i.locations"
-  for a in $(seq 1 $(cat $i.locations|wc -l)); do l=$(cat $i.locations|head -n$a|tail -n1|hxselect -c div|html2text -width 1000); echo "$l"; done> $i.parsedlocations
+  for a in $(seq 1 $(cat $i.locations|wc -l)); do l=$(cat $i.locations|head -n$a|tail -n1|hxselect -c div|html2text -utf8 -width 1000); echo "$l"; done> $i.parsedlocations
   for a in $(seq 1 $(cat $i.names|wc -l)); do 
     name=$(cat $i.parsednames|head -n$a|tail -n1); 
     loc=$(cat $i.parsedlocations|head -n$a|tail -n1); 
