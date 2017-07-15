26782ff0800af69d21b512365c671108eb33450f
Marcos Marado <mindboosternoori@gmail.com>
2017-07-15 18:35:03 +0100

    extract number of pages instead of magic number

--- dhcenters.sh
+++ dhcenters.sh
@@ -5,8 +5,10 @@ if ! command -v hxnormalize >/dev/null 2>&1 ; then
 fi
 
 # Actual work
+lastpage=$(wget http://dhcenternet.org/centers -o /dev/null -O -|grep "last page"|sed 's/last\ page/\n/'|tail -1|sed 's/page=/\n/'|tail -1|cut -d\" -f1)
+
 echo "Center,Location" > dhcenters.csv
-for i in $(seq 0 6); do 
+for i in $(seq 0 $lastpage); do
   wget "http://dhcenternet.org/centers?page=$i" -O "$i.html" 
   cat $i.html |hxnormalize -x -l 1000|hxselect tbody|hxselect div.views-field-realname|hxselect span.field-content|sed 's/<span/\n\<span/g'|grep -v ^$ > "$i.names"
   for a in $(seq 1 $(cat $i.names|wc -l)); do cat $i.names|head -n$a|tail -n1|hxselect -c a|html2text -width 1000; done > $i.parsednames
